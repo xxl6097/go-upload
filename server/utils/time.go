f9de1fa5884e6cb4a96861223701c0ae684838a4
MacBookAirM2 <uuxia@macbookairm2.com>
2024-01-04 20:35:00 +0800

    2024-01-04 20:35:00 by uuxia

--- server/utils/time.go
+++ server/utils/time.go
@@ -15,23 +15,28 @@ func TimeFormat(date time.Time, pattern string) string {
 
 // "2023-05-29 15:10:41"
 func GetNowStr() string {
-	loc, _ := time.LoadLocation("Asia/Shanghai")
-	return time.Now().In(loc).Format("2006-01-02 15:04:05")
+	//loc, _ := time.LoadLocation("Asia/Shanghai")
+	//return time.Now().In(loc).Format("2006-01-02 15:04:05")
+	return GetTimeFormat("2006-01-02 15:04:05")
 }
 
 func GetFileNameWithTime() string {
-	loc, _ := time.LoadLocation("Asia/Shanghai")
-	return time.Now().In(loc).Format("20060102150405")
+	//loc, _ := time.LoadLocation("Asia/Shanghai")
+	//return time.Now().In(loc).Format("20060102150405")
+	return GetTimeFormat("20060102150405")
 }
 
-func GetTimeDir() string {
-	//loc, _ := time.LoadLocation("Asia/Shanghai")
+func GetTimeFormat(format string) string {
 	location, err := time.LoadLocation(timezone)
 	if err != nil {
 		location = time.FixedZone("CST", 8*3600) //替换上海时区方式
 	}
 	date := time.Now()
 	date.In(location)
-	return date.Format("2006/01/02/15/04/05/")
+	return date.Format(format)
 
+}
+
+func GetTimeDir() string {
+	return GetTimeFormat("2006/01/02/15/04/05/")
 }
