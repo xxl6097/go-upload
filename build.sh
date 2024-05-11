c99ed9c0292659f87698c3a6e0eb64694a2e82e2
MacBookAirM2 <uuxia@macbookairm2.com>
2024-05-11 09:57:15 +0800

    1.1.6

--- build.sh
+++ build.sh
@@ -68,6 +68,7 @@ function build_linux_arm64() {
 
 function build_darwin_arm64() {
   #  CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ${appname}
+  echo "build macos arm64 $(GetLDFLAGS)"
   docker_push_result=$(CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags $(GetLDFLAGS) -o ${appname} 2>&1)
 }
 
