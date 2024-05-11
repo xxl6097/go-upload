74f8a3d07d81f765d89d27f9dc32ab3196fede6e
MacBookAirM2 <uuxia@macbookairm2.com>
2024-05-11 10:00:26 +0800

    1.1.6

--- build.sh
+++ build.sh
@@ -72,7 +72,7 @@ function build_darwin_arm64() {
   #  CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ${appname}
   # echo "build macos arm64 $(GetLDFLAGS)"
   #  docker_push_result=$(CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags $(GetLDFLAGS) -o ${appname} 2>&1)
-  CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags $(GetLDFLAGS) -o ${appname}
+  CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "$(GetLDFLAGS)" -o ${appname}
 }
 
 function build_images_to_tencent() {
